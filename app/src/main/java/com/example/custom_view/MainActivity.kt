541333b17b14db73b882e11cbead8fd1dc86033b
Sergey Vasilevkin <svasilevkin@gmail.com>
2020-03-02 22:21:57 +0300

    Implement RoundButtonView and styleable.

--- app/src/main/java/com/example/custom_view/MainActivity.kt
+++ app/src/main/java/com/example/custom_view/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.custom_view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+    
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
