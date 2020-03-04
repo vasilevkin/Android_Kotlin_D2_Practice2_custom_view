6954f550d571a0767afda54da2f9de1133852080
Sergey Vasilevkin <svasilevkin@gmail.com>
2020-03-04 22:42:04 +0300

    Implement main activity and setOnClickListener.

--- app/src/main/java/com/example/custom_view/MainActivity.kt
+++ app/src/main/java/com/example/custom_view/MainActivity.kt
@@ -2,12 +2,24 @@ package com.example.custom_view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
-    
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        left_toggle_button.setOnClickListener {
+            right_checkable_button.isEnabled = !right_checkable_button.isEnabled
+        }
+
+        right_checkable_button.setOnClickListener {
+            when (smile_view.state) {
+                RoundButtonView.ON -> smile_view.state = RoundButtonView.OFF
+                RoundButtonView.OFF -> smile_view.state = RoundButtonView.ON
+            }
+        }
     }
 }
